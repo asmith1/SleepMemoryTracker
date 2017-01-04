3d6dfdc44fb5f5d5b77644792e6e18530b0b6268
Ashley Smith <Ashley@Ashleys-MacBook-Pro-6.local>
2017-01-03 20:14:44 -0500

    made the buttons do stuff

--- SleepMemoryTracker/ViewController.swift
+++ SleepMemoryTracker/ViewController.swift
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to Ashlanda's First App", message: "Look! We made the app do something when you press buttons! Woooooooo! 😝 🤗", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
